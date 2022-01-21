eb93c615683970a696230a50b988eb432f03dbd7
guilhermow <gui.crb0@gmail.com>
2022-01-21 12:36:39 -0300

    connecting to a server successfulyy

--- flutterchat_server/server.js
+++ flutterchat_server/server.js
@@ -1,13 +1,36 @@
 const express = require('express')
 const { listen } = require('socket.io')
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 4000
 
-const server = app.listen(PORT, () => {
+const s = app.listen(PORT, () => {
     console.log('Server is listening.')
 })
 
-const serverIO = require('socket.io')(server)
+const server = require('socket.io')(s)
 
 const users = {}
 const rooms = {}
+
+server.on("connection", (socket) => {
+    console.log("Server has connected to a new socket.")
+    socket.emit('connection', "Socket has connected to a server.")
+    socket.on('disconnect', () => {
+        console.log('A socket has disconnected from the server.')
+    })
+})
+
+server.on("validate", (inData, inCallback) => {
+    const user = users[inData.userName]
+    if(user) {
+        if(user.password == inData.password){
+            inCallback({ status : "ok" })
+        } else {
+            inCallback({ status : "fail" })
+        }
+    } else {
+        users[inData.userName] = inData;
+        server.broadcast.emit("newUser", users)
+        inCallback({ status: "created" })
+    }
+})
